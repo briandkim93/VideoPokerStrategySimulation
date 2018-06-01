a47b7d52c9663bfe2c16c12e1daca6df585e326e
Brian Kim <briandkim93@gmail.com>
2018-05-31 18:05:56 -0700

    Removed unnecessary semicolons after for loops.

--- simulation.js
+++ simulation.js
@@ -11,7 +11,7 @@ function buildDeck(deck) {
         deck.push(new Card(i, 'D'));
         deck.push(new Card(i, 'H'));
         deck.push(new Card(i, 'S'));
-    };
+    }
 };
 
 function shuffleDeck(deck) {
@@ -24,14 +24,14 @@ function shuffleDeck(deck) {
 function deal(deck, hand) {
     for (let i = 0; i < 5; i += 1) {
         hand.push(deck.shift());
-    };
+    }
 };
 
 function draw(deck, hand, discardIndexes) {
     discardIndexes.sort(function(a, b) {return (b-a)});
     for (let index of discardIndexes) {
         hand.splice(index, 1);
-    };
+    }
     for (let i = 0; i < discardIndexes.length; i += 1) {
         hand.push(deck.shift());
     }
