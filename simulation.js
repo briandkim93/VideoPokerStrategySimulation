064bd3b9ce0a8d258626e56ae837e7406afa16d4
Brian Kim <briandkim93@gmail.com>
2018-06-02 15:59:25 -0700

    Added functionality for checkpoints 1-9 of the simple strategy.

--- simulation.js
+++ simulation.js
@@ -27,20 +27,41 @@ function deal(deck, hand) {
     }
 };
     
-function draw(deck, hand, discardIndexes) {
-    discardIndexes.sort((a, b) => b - a);
-    for (let index of discardIndexes) {
-        hand.splice(index, 1);
-    }
-    for (let i = 0; i < discardIndexes.length; i += 1) {
+function draw(deck, hand) {
+    for (let i = hand.length; hand.length < 5; i += 1) {
         hand.push(deck.shift());
     }
 }
 
-function checkDiscardable(hand) {
-    const discardIndexes = [];
-    // ADDITIONAL CODE REQUIRED
-    return discardIndexes; 
+function holdStrategy(hand) {
+    if (checkOne(hand)) {
+        return checkOne(hand);
+    }
+    if (checkTwo(hand)) {
+        return checkTwo(hand);
+    }
+    if (checkThree(hand)) {
+        return checkThree(hand);
+    }
+    if (checkFour(hand)) {
+        return checkFour(hand);
+    }
+    if (checkFive(hand)) {
+        return checkFive(hand);
+    }
+    if (checkSix(hand)) {
+        return checkSix(hand);
+    }
+    if (checkSeven(hand)) {
+        return checkSeven(hand);
+    }
+    if (checkEight(hand)) {
+        return checkEight(hand);
+    }
+    if (checkNine(hand)) {
+        return checkNine(hand);
+    }
+    return [];
 }
 
 function checkHand(hand) {
@@ -99,9 +120,9 @@ function playGame(balance) {
     buildDeck(newDeck);
     shuffleDeck(newDeck);
     deal(newDeck, newHand);
-    const cardsToDiscard = checkDiscardable(newHand);
-    draw(newDeck, newHand, cardsToDiscard);
-    payout = checkHand(newHand);
+    const heldCards = holdStrategy(newHand);
+    draw(newDeck, heldCards);
+    payout = checkHand(heldCards);
     return payout;
 }
 
@@ -120,10 +141,161 @@ function simulation(n) {
     return payoutPercentage + '%';
 }
 
+function checkOne(hand) {
+    if (checkRoyalFlush(hand) || checkStraightFlush(hand) || checkFourOfAKind(hand)) {
+        return hand;
+    } else {
+        return false;
+    }
+}
+
+function checkTwo(hand) {
+    const maxSuitInHand = maxSuit(hand).maxSuitInHand;
+    const maxSuitOccurrence = maxSuit(hand).maxSuitOccurrence;
+
+    if (maxSuit(hand).maxSuitOccurrence === 4) {
+        for (let i = 0; i < hand.length; i += 1) {
+            var heldCards = hand.filter(function(card){return card.suit === maxSuitInHand;});
+        }
+        if (intersection(heldCards.map(card => card.number), [1,10,11,12,13]).length === 4) {
+            return heldCards;
+        }
+    }
+    return false;
+}
+
+function checkThree(hand) {
+    if (checkStraight(hand) || checkFlush(hand) || checkFullHouse(hand)) {
+        return hand;
+    } else if (checkThreeOfAKind(hand)) {
+        const elementCountObject = elementCounter(hand.map(card => card.number));
+        for (number in elementCountObject) {
+            if (elementCountObject[number] === 3) {
+                var threeOfAKindNumber = parseInt(number);
+            }
+        }
+        const heldCards = hand.filter(function(card){return card.number === threeOfAKindNumber;});
+        return heldCards;
+    } else {
+        return false;
+    }
+}
+
+function checkFour(hand) {
+    const maxSuitInHand = maxSuit(hand).maxSuitInHand;
+    const maxSuitOccurrence = maxSuit(hand).maxSuitOccurrence;
+
+    if (maxSuit(hand).maxSuitOccurrence === 4) {
+        for (let i = 0; i < hand.length; i += 1) {
+            if (hand[i].suit != maxSuitInHand) {
+                hand.splice(i, 1);
+            }
+        }
+        for (let i = 1; i <= 9; i += 1) {
+            const straightArray = [i, i + 1, i + 2, i + 3, i + 4];
+        if (intersection(hand.map(card => card.number), straightArray).length === 4) {
+            return hand;
+        }
+        }
+    }
+    return false;
+}
+
+function checkFive(hand) {
+    if (checkTwoPair(hand)) {
+        const elementCountObject = elementCounter(hand.map(card => card.number));
+        for (number in elementCountObject) {
+            if (elementCountObject[number] === 1) {
+                var toDiscard = parseInt(number);
+            }
+        }
+        const heldCards = hand.filter(function(card){return card.number !== toDiscard;});
+        return heldCards;
+    } else {
+        return false;
+    }
+}
+
+function checkSix(hand) {
+    if (checkHighPair(hand)) {
+        const elementCountObject = elementCounter(hand.map(card => card.number));
+        var heldCards = hand;
+        for (number in elementCountObject) {
+            if (elementCountObject[number] === 1) {
+                var toDiscard = parseInt(number);
+                heldCards = heldCards.filter(function(card){return card.number !== toDiscard;});
+            }
+        }
+        return heldCards;
+    } else {
+        return false;
+    }
+}
+
+function checkSeven(hand) {
+    const maxSuitInHand = maxSuit(hand).maxSuitInHand;
+    const maxSuitOccurrence = maxSuit(hand).maxSuitOccurrence;
+
+    if (maxSuit(hand).maxSuitOccurrence === 3) {
+        for (let i = 0; i < hand.length; i += 1) {
+            heldCards = hand.filter(function(card){return card.suit === maxSuitInHand;});
+        }
+        if (intersection(heldCards.map(card => card.number), [1,10,11,12,13]).length === 3) {
+            return heldCards;
+        }
+    }
+    return false;
+}
+
+function checkEight(hand) {
+    const maxSuitInHand = maxSuit(hand).maxSuitInHand;
+    const maxSuitOccurrence = maxSuit(hand).maxSuitOccurrence;
+
+    if (maxSuit(hand).maxSuitOccurrence === 4) {
+        for (let i = 0; i < hand.length; i += 1) {
+            heldCards = hand.filter(function(card){return card.suit === maxSuitInHand;});
+        }
+        return heldCards;
+    }
+    return false;
+}
+
+function checkNine(hand) {
+    if (checkLowPair(hand)) {
+        const elementCountObject = elementCounter(hand.map(card => card.number));
+        var heldCards = hand;
+        for (number in elementCountObject) {
+            if (elementCountObject[number] === 1) {
+                var toDiscard = parseInt(number);
+                heldCards = heldCards.filter(function(card){return card.number !== toDiscard;});
+            }
+        }
+        return heldCards;
+    } else {
+        return false;
+    }
+}
+
+function checkLowPair(hand) {
+    const handNumbers = hand.map(card => card.number);
+    const elementCountArray = Object.values(elementCounter(handNumbers));
+    if (elementCountArray.filter(function(count){return count === 2}).length === 1) {
+        const handNumbersString = handNumbers.toString();
+        for (let i = 2; i < 11; i += 1) {
+            if (handNumbersString.includes(i + ',' + i)) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
+
 function checkHighPair(hand) {
     const handNumbers = hand.map(card => card.number);
-    const numberCountArray = numberCounter(handNumbers);
-    if (numberCountArray.filter(function(count){return count === 2}).length === 1) {
+    const elementCountArray = Object.values((elementCounter(handNumbers)));
+    if (elementCountArray.filter(function(count){return count === 2}).length === 1) {
         const handNumbersString = handNumbers.toString();
         if (handNumbersString.includes('1,1') ||
             handNumbersString.includes('11,11') ||
@@ -138,8 +310,8 @@ function checkHighPair(hand) {
 
 function checkTwoPair(hand) {
     const handNumbers = hand.map(card => card.number);
-    const numberCountArray = numberCounter(handNumbers);
-    if (numberCountArray.filter(function(count){return count === 2}).length === 2) {
+    const elementCountArray = Object.values(elementCounter(handNumbers));
+    if (elementCountArray.filter(function(count){return count === 2}).length === 2) {
         return true;
     } else {
         return false;
@@ -148,8 +320,8 @@ function checkTwoPair(hand) {
 
 function checkThreeOfAKind(hand) {
     const handNumbers = hand.map(card => card.number);
-    const numberCountArray = numberCounter(handNumbers);
-    if (numberCountArray.filter(function(count){return count === 3}).length) {
+    const elementCountArray = Object.values(elementCounter(handNumbers));
+    if (elementCountArray.filter(function(count){return count === 3}).length) {
         return true;
     } else {
         return false;
@@ -162,7 +334,7 @@ function checkStraight(hand) {
     if (checkRoyalStraight(hand)) {
         return true;
     }
-    for (let i = 0; i < 4; i += 1) {
+    for (let i = 0; i < handNumbers.length - 1; i += 1) {
         if (handNumbers[i] + 1 == handNumbers[i + 1]) {
             continue;
         } else {
@@ -171,6 +343,7 @@ function checkStraight(hand) {
     }
     return true;
 }
+
 function checkRoyalStraight(hand) {
     const handNumbers = hand.map(card => card.number);
     handNumbers.sort((a, b) => a - b);
@@ -196,9 +369,9 @@ function checkFlush(hand) {
 
 function checkFullHouse(hand) {
     const handNumbers = hand.map(card => card.number);
-    const numberCountArray = numberCounter(handNumbers);
-    if (numberCountArray.filter(function(count){return count === 2}).length &&
-        numberCountArray.filter(function(count){return count === 3}).length) {
+    const elementCountArray = Object.values(elementCounter(handNumbers));
+    if (elementCountArray.filter(function(count){return count === 2}).length &&
+        elementCountArray.filter(function(count){return count === 3}).length) {
         return true;
     } else {
         return false;
@@ -208,8 +381,8 @@ function checkFullHouse(hand) {
 
 function checkFourOfAKind(hand) {
     const handNumbers = hand.map(card => card.number);
-    const numberCountArray = numberCounter(handNumbers);
-    if (numberCountArray.filter(function(count){return count === 4}).length) {
+    const elementCountArray = Object.values(elementCounter(handNumbers));
+    if (elementCountArray.filter(function(count){return count === 4}).length) {
         return true;
     } else {
         return false;
@@ -232,11 +405,36 @@ function checkRoyalFlush(hand) {
     }
 }
 
-function numberCounter(handNumbers) {
-    handNumbers.sort((a, b) => a - b);
-    const numberCountObject = {};
-    for (let i = 0; i < 5; i += 1) {
-        numberCountObject[handNumbers[i]] = (numberCountObject[handNumbers[i]] || 0) + 1;
+function elementCounter(array) {
+    const elementCountObject = {};
+    for (let i = 0; i < array.length; i += 1) {
+        elementCountObject[array[i]] = (elementCountObject[array[i]] || 0) + 1;
     }
-    return Object.values(numberCountObject);
+    return elementCountObject;
+}
+
+function maxSuit(hand) {
+    const handSuits = hand.map(card => card.suit);
+    const suitCountObject = elementCounter(handSuits);
+    const maxSuitObject = {
+        maxSuitOccurrence: 0,
+        maxSuitInHand: ''
+    }
+    for (let suit in suitCountObject) {
+        if (suitCountObject[suit] > maxSuitObject.maxSuitOccurrence) {
+            maxSuitObject.maxSuitOccurrence = suitCountObject[suit];
+            maxSuitObject.maxSuitInHand = suit;
+        }
+    }
+    return maxSuitObject;
+}
+
+function intersection(array1, array2) {
+    const intersectionArray = [];
+    for (let i = 0; i < array1.length; i += 1) {
+        if (array2.includes(array1[i])) {
+            intersectionArray.push(array1[i]);
+        }
+    }
+    return intersectionArray;
 }
